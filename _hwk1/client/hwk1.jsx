a82cdee308b3a701886d2c3a38d67ea43a925a3b
Joseph Orbegoso Pea <joe@trusktr.io>
2015-07-18 01:40:18 -0700

    Got gasket working.

--- _hwk1/client/hwk1.jsx
+++ _hwk1/client/hwk1.jsx
@@ -1,6 +1,5 @@
 
 Template.hwk1.onRendered(()=>{
-    console.log('Hello WebGl!')
 
     let canvas = document.getElementById( "gl-canvas" );
 
@@ -35,31 +34,67 @@ Template.hwk1.onRendered(()=>{
     let program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
 
-    // define some vertices
-    let vertices = new Float32Array([
-        0, -0.9,
-        0.5, 0.9,
-        0.9, -0.9,
-        -0.9, -0.5,
-        0, 0.9,
-        -0.4, -0.9
-    ]);
+    //
+    //  Initialize our data for the Sierpinski Gasket
+    //
 
-    // Load the first set of vertices into the GPU
-    let bufferId = gl.createBuffer();
+    // First, initialize the corners of our gasket with three points.
+
+    let points = [];
+    let NumTimesToSubdivide = 5;
+    let vertices = [
+        vec2( -1, -1 ),
+        vec2(  0,  1 ),
+        vec2(  1, -1 )
+    ];
+
+    divideTriangle( vertices[0], vertices[1], vertices[2], NumTimesToSubdivide);
+
+    // Load the data into the GPU
+
+    var bufferId = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-    gl.bufferData( gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
 
-    // Associate our shader variables with our data buffer
-    let vPosition = gl.getAttribLocation( program, "vPosition" );
+    // Associate out shader variables with our data buffer
+
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
     gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPosition );
 
-    // render the first vertices.
     render();
 
     function render() {
         gl.clear( gl.COLOR_BUFFER_BIT );
-        gl.drawArrays( gl.TRIANGLES, 0, 6 );
+        gl.drawArrays( gl.TRIANGLES, 0, points.length );
+    }
+
+    function triangle( a, b, c ) {
+        points.push( a, b, c );
+    }
+
+    function divideTriangle( a, b, c, count ) {
+
+        // check for end of recursion
+
+        if ( count === 0 ) {
+            triangle( a, b, c );
+        }
+        else {
+
+            //bisect the sides
+
+            var ab = mix( a, b, 0.5 );
+            var ac = mix( a, c, 0.5 );
+            var bc = mix( b, c, 0.5 );
+
+            --count;
+
+            // three new triangles
+
+            divideTriangle( a, ab, ac, count );
+            divideTriangle( c, ac, bc, count );
+            divideTriangle( b, bc, ab, count );
+        }
     }
 })
